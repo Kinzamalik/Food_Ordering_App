c5a2c174f723f1888b3db2c3019ed91200eb0531
Kinzamalik <kiinza.malik@gmail.com>
2021-12-26 22:19:15 +0500

    some fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
- import { Fragment, useState } from "react";
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
+import { useState } from 'react';
 
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
+
   const showCartHandler = () => {
     setCartIsShown(true);
   };
@@ -13,14 +15,15 @@ function App() {
   const hideCartHandler = () => {
     setCartIsShown(false);
   };
+
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header  onShowCart={showCartHandler}/>
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
